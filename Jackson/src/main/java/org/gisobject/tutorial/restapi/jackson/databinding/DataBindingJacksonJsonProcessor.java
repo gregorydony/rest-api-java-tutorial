ec6350d2796ab316bed49871e029ff97a70e7fe4
gregorydony <gregorydony@gmail.com>
2017-09-29 11:00:47 +0200

    #3 Implement Jackson Data binding

---
+++ Jackson/src/main/java/org/gisobject/tutorial/restapi/jackson/databinding/DataBindingJacksonJsonProcessor.java
@@ -0,0 +1,56 @@
+package org.gisobject.tutorial.restapi.jackson.databinding;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.gisobject.tutorial.restapi.bean.Employee;
+import org.gisobject.tutorial.restapi.json.EmployeeJsonProcessor;
+import org.gisobject.tutorial.restapi.json.JsonProcessor;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by GIS Object on 29/09/2017.
+ */
+public enum DataBindingJacksonJsonProcessor implements EmployeeJsonProcessor, JsonProcessor<Employee> {
+
+    DATA_BINDING_JACKSON_JSON_PROCESSOR;
+
+    ObjectReader objectReader;
+
+    ObjectWriter objectWriter;
+
+    DataBindingJacksonJsonProcessor() {
+        DateFormat dateFormat =  new SimpleDateFormat(HIRE_DATE_PATTERN);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectReader = objectMapper.reader();
+        objectWriter = objectMapper.writer(dateFormat);
+    }
+
+    @NotNull
+    public List<Employee> readJson(@NotNull InputStream inputStream) {
+        try {
+            return objectReader.forType(new TypeReference<List<Employee>>(){}).readValue(inputStream);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    @Override
+    public void writeJson(List<Employee> employees, OutputStream os) {
+        try {
+            objectWriter.writeValue(os, employees);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+
+}
